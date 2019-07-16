8462a0b92446dd7a9f14c89e454656dfe9ba7578
DiiegoFrancisco <difrancisco2101@gmail.com>
2019-07-16 17:57:02 -0300

    Anotacoes

---
+++ Anotacoes/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Anotacoes
+//
+//  Created by DevMakerMobile on 16/07/2019.
+//  Copyright © 2019 DevMakerMobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lbText: UITextView!
+    let chave = "Anotacao"
+    
+    @IBAction func btSave(_ sender: Any) {
+        
+       if let text = lbText.text {
+            UserDefaults.standard.set(text, forKey: chave)
+        }
+        
+    }
+    
+    func recoverText() -> String{
+    if let recoverText = UserDefaults.standard.object(forKey: chave){
+        return recoverText as! String
+        
+    }
+        return ""
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       lbText.text = recoverText()
+    }
+
+
+}
+
+
